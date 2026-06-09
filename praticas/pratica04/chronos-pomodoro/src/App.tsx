22b79a382330b0f66e8465b2c6fd9faf2a6c9d3c
VMT20 <vinicius.massaranduba@iesb.edu.br>
2026-06-09 11:12:58 -0300

    Initialize projeto-react

---
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles/theme.css";
+import "./styles/globals.css";
+import { Heading } from "./components/Heading";
+import { Timer } from "lucide-react"; // Importando o ícone
+
+export function App() {
+  return (
+    <>
+      <Heading>
+        Histórico
+        <button>
+          <Timer />
+        </button>
+      </Heading>
+    </>
+  );
+}
