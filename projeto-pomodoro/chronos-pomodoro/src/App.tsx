34b481ee17461443bc4e47677fe533e2d504744d
VMT20 <vinicius.massaranduba@iesb.edu.br>
2026-06-09 11:12:37 -0300

    Initialize feature/pratica22

---
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,71 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import './styles/theme.css';
+import './styles/global.css';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
+import { useState } from 'react';
+
+export function App() {
+  // Que todos os componentes que usam "numero"
+  // saibam das mudanças em seu valor.
+  // Sempre que eu usar useState, não vou usar atribuição diretamente
+  // const [numero, setNumero] = useState(() => {
+  //   console.log('Lazy initialization');
+  //   return 0;
+  // });
+  const [numero, setNumero] = useState(0);
+
+  function handleClick() {
+    // setNumero(prevState => prevState + 1);
+    setNumero(1);
+  }
+  return (
+    <>
+      <Heading>Número: {numero}</Heading>
+      <button onClick={handleClick}>Aumenta</button>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText={numero.toString()}
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
+}
