3f06df2e6b2f053e163a79bfdc009e35ecf64e8d
VMT20 <vinicius.massaranduba@iesb.edu.br>
2026-06-09 11:13:06 -0300

    Initialize feature/pratica39

---
+++ projeto-pomodoro/chronos-pomodoro/src/components/MainForm/index.tsx
@@ -0,0 +1,55 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+
+export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('DEU CERTO');
+  }
+  const { setState } = useTaskContext();
+
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        formattedSecondsRemaining: '21:00',
+      };
+    });
+  }
+
+  return (
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
+      <button onClick={handleClick} type='button'>
+        Clicar
+      </button>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+          ref={taskNameInput}
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Próximo intervalo é de 25min</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
